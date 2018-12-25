ae8443f96837141c90ea1ecd464a5afeb45a01e2
Shai Nagar <shain@wix.com>
2018-12-25 15:48:41 +0200

    a code change

--- src/scala/com/second/party/example/scala/SecondPartyExample.scala
+++ src/scala/com/second/party/example/scala/SecondPartyExample.scala
@@ -1,6 +1,6 @@
 package com.second.party.example.scala
 
 object SecondPartyExample extends App {
-
   System.out.println("I'm a second party example...")
+  System.out.println("My lib name is 2nd_party_example_8af7967a")
 }
