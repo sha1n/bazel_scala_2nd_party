65ff095e9b09700e91553931dcba82772552bfe2
Shai Nagar <shain@wix.com>
2018-12-25 16:24:41 +0200

    commit #3

--- src/scala/com/second/party/example/scala/SecondPartyExample.scala
+++ src/scala/com/second/party/example/scala/SecondPartyExample.scala
@@ -1,6 +1,10 @@
 package com.second.party.example.scala
 
 object SecondPartyExample extends App {
+  // Initial commit
   System.out.println("I'm a second party example...")
+  // Commit #2
   System.out.println("My lib name is 2nd_party_example_8af7967a")
+  // Commit #3
+  System.out.println("another change")
 }
