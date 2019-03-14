84ebe974b218f381e560416b8a6447858602ef43
Shai Nagar <shain@wix.com>
2019-03-14 09:38:48 +0200

    testing basic proto functionality

--- src/com/second/party/example/flat/FlatSecondPartyExample.scala
+++ src/com/second/party/example/flat/FlatSecondPartyExample.scala
@@ -1,12 +1,16 @@
 package com.second.party.example.flat
 
+import com.second.party.example.proto.api.Identifiable
+
 object FlatSecondPartyExample extends App {
   // Initial commit
-  System.out.println("I'm a second party example...")
+  println("I'm a second party example...")
   // Commit #2
-  System.out.println("My lib name is 2nd_party_example_8af7967a")
+  println("My lib name is 2nd_party_example_8af7967a")
   // Commit #3
-  System.out.println("another change")
+  println("another change")
+
+  Identifiable(id = "1", name = "name")
 
   def aMethod(p: String): Unit = {}
 }
--- src/scala/com/second/party/example/scala/SecondPartyExample.scala
+++ src/scala/com/second/party/example/scala/SecondPartyExample.scala
@@ -2,11 +2,11 @@ package com.second.party.example.scala
 
 object SecondPartyExample extends App {
   // Initial commit
-  System.out.println("I'm a second party example...")
+  println("I'm a second party example...")
   // Commit #2
-  System.out.println("My lib name is 2nd_party_example_8af7967a")
+  println("My lib name is 2nd_party_example_8af7967a")
   // Commit #3
-  System.out.println("another change")
+  println("another change")
 
   def aMethod(p: String): Unit = {}
 }
